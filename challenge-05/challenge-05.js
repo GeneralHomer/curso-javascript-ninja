029828a38acdc52045ec35e361c882e64081477a
Homero Oliveira <39320595+GeneralHomer@users.noreply.github.com>
2020-06-12 19:47:05 -0300

    Update challenge-05.js

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,19 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var val = [1,'array','prefiro criar um obj',true,71];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+ function retorneArr(arr){
+... return val;
+... }
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+retorneArr()[1];
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,7 +22,7 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
